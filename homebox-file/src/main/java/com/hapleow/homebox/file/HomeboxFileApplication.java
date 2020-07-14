683242da9bf33067f7aecd190465d7632b3309cc
hapleow <hapleow@gmail.com>
2020-07-14 10:41:56 +0800

    add file module.

---
+++ homebox-file/src/main/java/com/hapleow/homebox/file/HomeboxFileApplication.java
@@ -0,0 +1,13 @@
+package com.hapleow.homebox.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeboxFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeboxFileApplication.class, args);
+    }
+
+}
---
+++ homebox-file/src/test/java/com/hapleow/homebox/file/HomeboxFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hapleow.homebox.file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeboxFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
