845bfceb0c6e705d71eb65328d563b0a68406baf
hapleow <hapleow@gmail.com>
2020-07-15 17:05:22 +0800

    homebox-codge init.

---
+++ homebox-codge/src/main/java/com/hapleow/homeboxcodge/HomeboxCodgeApplication.java
@@ -0,0 +1,13 @@
+package com.hapleow.homeboxcodge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeboxCodgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeboxCodgeApplication.class, args);
+    }
+
+}
---
+++ homebox-codge/src/test/java/com/hapleow/homeboxcodge/HomeboxCodgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hapleow.homeboxcodge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeboxCodgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
