3ee4462c7b27cd05ef290e6e5abb8865ab9eb757
HapLeo <hapleow@gmail.com>
2020-07-15 23:11:34 +0800

    dir read.

--- homebox-file/src/main/java/com/hapleow/homebox/file/service/impl/FileCopyServiceImpl.java
+++ homebox-file/src/main/java/com/hapleow/homebox/file/service/impl/FileCopyServiceImpl.java
@@ -10,6 +10,8 @@ import com.hapleow.homebox.file.service.IFileService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+
 /**
  * @author wuyulin
  * @date 2020/7/14
@@ -21,8 +23,42 @@ public class FileCopyServiceImpl implements IFileService {
 
     @Override
     public void copyAll(FileCopyAllDto fileCopyAllDto) {
-
         log.info("参数列表：" + JSON.toJSONString(fileCopyAllDto));
+        // 递归获取指定目录下的所有文件路径，保存到redis
+        saveToRedis(fileCopyAllDto);
+    }
+
+    /**
+     * 保存文件路径到redis
+     * @param fileCopyAllDto
+     */
+    private void saveToRedis(FileCopyAllDto fileCopyAllDto) {
+        String fromPath = fileCopyAllDto.getFromPath();
+        File fromDir = new File(fromPath);
+        recursionSave(fromDir);
+
+
+    }
+
+    /**
+     * 递归保存
+     * @param fromDir
+     */
+    private void recursionSave(File fromDir) {
+
+        if (!fromDir.exists()){
+            throw new IllegalArgumentException("来源路径不存在！" + fromDir.getAbsolutePath());
+        }
+        File[] files = fromDir.listFiles();
+        for (File file : files) {
+            if (file.isFile()){
+                // 保存到redis
+
+            }else {
+                // 递归调用
+                recursionSave(file);
+            }
+        }
     }
 
     /**
