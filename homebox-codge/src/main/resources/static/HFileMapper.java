f8f7f588a30b124035c432c8330380bcc6e97b55
HapLeo <hapleow@gmail.com>
2020-07-19 23:29:11 +0800

    生成templates目录下所有模板的对应文件

--- homebox-codge/src/main/resources/static/HFileMapper.java
+++
@@ -1,9 +0,0 @@
-/**
- * 文件表
- * @author wyl
- * @date 2020-07-19T21:26:15.265426500
- */
-public interface HFileMapper {
-
-    List<HFile> list(@Param("HFile") HFile hFile);
-}
--- homebox-codge/src/main/resources/static/HUserMapper.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- * @author wyl
- * @date 2020-07-19T21:26:38.995246400
- */
-public interface HUserMapper {
-
-    List<HUser> list(@Param("HUser") HUser hUser);
-}
